bcc9d4cf5ac5bc2c42792e93b5947c1d2043d71c
Ngashie <ngashdavid95@gmail.com>
2023-01-17 11:29:07 +0300

    pg_hba.conf file

--- Posterity/models.py
+++ Posterity/models.py
@@ -1,5 +1,9 @@
 from django.dg import models
 
 class Members(models.Model):
-    lname = models.CharField(max_length=5)
+    lname = models.CharField(max_length=10)
     password = models.Charfield(max_length=8)
+
+class Event(models.Model):
+    eventname = models.CharField(max_length=20)
+    vibes = models.ImageField()
--- Posterity/settings.py
+++ Posterity/settings.py
@@ -78,10 +78,14 @@ WSGI_APPLICATION = 'Posterity.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'postgres',
+        'PASSWORD': 'pewpew',
+        'HOST': 'localhost',
+        'PORT': 5432
+        }
     }
-}
 
 
 # Password validation
